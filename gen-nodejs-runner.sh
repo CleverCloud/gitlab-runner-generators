c9b70922418e512c79d953e33d8e5e46ff6db7fc
Fabrice Luraine <16@asciiland.net>
2018-09-18 15:46:51 +0200

    Add missing python3 dependency
    Required to run the web server at the end of the `go.sh` script

--- gen-nodejs-runner.sh
+++ gen-nodejs-runner.sh
@@ -71,7 +71,7 @@ RUN chmod +x go.sh
 
 # Install gitlab-runner and nodejs
 RUN apt-get update && \\
-    apt-get install -y curl && \\
+    apt-get install -y curl python3 && \\
     curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash  && \\
     apt-get install -y gitlab-runner && \\
     curl -sL https://deb.nodesource.com/setup_9.x | bash  && \\
